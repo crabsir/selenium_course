ae7ec87a13895649ee182090698759ad29d27d8b
Crabsir <irbatir@gmail.com>
2020-04-17 19:02:54 +0300

    Full progress for the course thus far

---
+++ lesson2.2/lesson2.2_step5.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+link = "https://SunInJuly.github.io/execute_script.html"
+browser.get(link)
+
+button = browser.find_element_by_tag_name("button")
+
+# browser.execute_script("return arguments[0].scrollIntoView({block: 'center'});", button)
+browser.execute_script("return window.scrollBy(0, 150);")
+
+button.click()
+
+assert True
